6c5749fe39b808136365453732b7d6b6780a197c
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2023-03-15 08:54:55 +0100

    Initialisation Portfolio

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+
+
+  <main class="bg-[#222] text-white">
+    <RouterView />
+  </main>
+
+
+</template>
+
+<style scoped>
+
+</style>
