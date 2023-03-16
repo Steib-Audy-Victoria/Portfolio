f0c8c80cbb9dfd0e025ad6ea67b0b75048e724e6
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2023-03-16 19:43:43 +0100

    page contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -9,4 +9,23 @@
             </li>
         </ul>
     </div>
+
+    <section class="py-4">
+        <form class="flex flex-col gap-10 justify-center items-center border-2 border-white p-10 mx-40" action="https://api.web3forms.com/submit" method="POST">
+
+            <input type="hidden" name="access_key" value="7910f823-6521-4306-bedd-b21809468bc9">
+
+            <p class="text-center font-semibold text-xl">Vous pouvez me contacter ici !</p>
+
+            <input class="w-1/2 px-2 placeholder:text-black bg-gray-100" type="text" name="name" placeholder="Votre prénom et/ou nom" required>
+            <input class="w-1/2 px-2 placeholder:text-black bg-gray-100" type="email" name="email" placeholder="Votre adresse-mail" required>
+            <textarea class="w-1/2 px-2 placeholder:text-black bg-gray-100" name="message" placeholder="Votre message" required></textarea>
+
+
+            <input type="hidden" name="redirect" value="https://web3forms.com/success">
+
+            <button class="border border-white p-2 font-bold " type="submit">Envoyer</button>
+
+        </form>
+    </section>
 </template>
